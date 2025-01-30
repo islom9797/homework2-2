e7ab8cb9edc80804432b16ed905dbcffef706618
islom9797 <rixsiyevvv@gmail.com>
2025-01-30 18:10:43 +0500

    third

--- src/main/java/org/islom/homework22/repository/ClientRepo.java
+++ src/main/java/org/islom/homework22/repository/ClientRepo.java
@@ -1,9 +1,10 @@
 package org.islom.homework22.repository;
 
 import org.islom.homework22.entity.Client;
+import org.islom.homework22.projection.CustomClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "product",collectionResourceRel = "list",excerptProjection = CustomAddress.class)
+@RepositoryRestResource(path = "product",collectionResourceRel = "list",excerptProjection = CustomClient.class)
 public interface ClientRepo extends JpaRepository<Client, Integer> {
 }
--- src/main/java/org/islom/homework22/repository/WareHouseRepo.java
+++ src/main/java/org/islom/homework22/repository/WareHouseRepo.java
@@ -1,9 +1,10 @@
 package org.islom.homework22.repository;
 
 import org.islom.homework22.entity.WareHouse;
+import org.islom.homework22.projection.CustomWareHouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "PcMarket",collectionResourceRel = "list",excerptProjection = CustomAddress.class)
+@RepositoryRestResource(path = "wareHouse",collectionResourceRel = "list",excerptProjection = CustomWareHouse.class)
 public interface WareHouseRepo extends JpaRepository<WareHouse,Integer> {
 }
